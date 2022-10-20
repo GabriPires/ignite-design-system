db38cf17983945bea1d13980c79bc49110da387d
Gabriel Pires <gabpiresdev@gmail.com>
2022-10-20 19:25:11 -0300

    chore: configure react package to start to build components

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@gabpires-ui/tokens'
-
-console.log('Hello world!', colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@gabpires-ui/tokens'
+
+export function App() {
+  return <h1 style={{ color: colors.ignite300 }}>Hello world</h1>
+}
