398561529bd5984c9e9c5fc09726aa4eb2f56fe4
Gabriel Pires <gabpiresdev@gmail.com>
2023-01-26 19:31:53 -0300

    docs to heading stories props

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -15,5 +15,12 @@ export const CustomTag: StoryObj<HeadingProps> = {
   args: {
     children: 'H1 Heading',
     as: 'h1'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Por padrão o heading sempre será um `h2`, mas podemos alterar isso com a propriedade `as`'
+      }
+    }
   }
 };
