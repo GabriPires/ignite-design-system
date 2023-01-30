87c51b0386a4faa3559098c0062d34ad08e84250
Gabriel Pires <gabpiresdev@gmail.com>
2023-01-30 20:18:06 -0300

    text and heading component control

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,17 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
-    children: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores molestias animi quis fugiat dolorem laborum, ad amet fuga sequi enim. Corporis ea voluptate veniam maiores dolorem quam accusantium perferendis quod?'
+    children: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores molestias animi quis fugiat dolorem laborum, ad amet fuga sequi enim. Corporis ea voluptate veniam maiores dolorem quam accusantium perferendis quod?',
+    size: 'md'
   },
+  argTypes: {
+    size: {
+      options: ['xxs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl', '4xl', '5xl', '6xl', '7xl', '8xl', '9xl'],
+      control: {
+        type: 'inline-radio'
+      }
+    },
+  }
 } as Meta<TextProps>;
 
 export const Primary: StoryObj<TextProps> = {};
